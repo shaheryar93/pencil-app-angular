b8034270a9e88d6f0be4acfb5a1f5eb7cb7c393d
shaheryar93 <shaheryar.shakil@walee.pk>
2022-01-04 00:39:38 +0500

    g

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/angular-test-pencil/'));
 
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname+"/dist/index.html"))
+    res.sendFile(path.join(__dirname+"/dist/angular-test-pencil/index.html"))
 
 });
 app.listen(process.env.PORT || 8080);
